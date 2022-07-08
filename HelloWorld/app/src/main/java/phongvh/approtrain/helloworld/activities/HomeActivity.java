df9369eafb206fc4dee1004d666461b0f77b3609
phongvanptit <phongvanptit@gmail.com>
2022-07-08 11:15:35 +0700

    Day2: Practice with Android UI

--- HelloWorld/app/src/main/java/phongvh/approtrain/helloworld/activities/HomeActivity.java
+++ HelloWorld/app/src/main/java/phongvh/approtrain/helloworld/activities/HomeActivity.java
@@ -3,6 +3,8 @@ package phongvh.approtrain.helloworld.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,9 +19,18 @@ public class HomeActivity extends Activity {
         Intent i = getIntent();
         TextView username = (TextView) findViewById(R.id.txt_username);
         TextView password = (TextView) findViewById(R.id.txt_password);
-
         username.setText(i.getStringExtra("user_name"));
         password.setText(i.getStringExtra("password"));
+        Button btnConfirm = (Button) findViewById(R.id.btn_confirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("confirm", "Login success");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
 
     }
 }
--- HelloWorld/app/src/main/java/phongvh/approtrain/helloworld/activities/LoginActivity.java
+++ HelloWorld/app/src/main/java/phongvh/approtrain/helloworld/activities/LoginActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import phongvh.approtrain.helloworld.R;
-import androidx.annotation.Nullable;
+
 public class LoginActivity extends Activity {
+    public final int LOGIN_REQUEST = 1000;
     public final String TAG = LoginActivity.class.getSimpleName();
+    private TextView txtSignIn, txtForget;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
-        Button btnGoto = (Button) findViewById(R.id.btn_goto);
-        btnGoto.setOnClickListener(new View.OnClickListener() {
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        txtSignIn = (TextView) findViewById(R.id.txt_result);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Using explicit intent
@@ -24,9 +32,18 @@ public class LoginActivity extends Activity {
                 Intent i = new Intent("ACTION_GOTO");
                 i.putExtra("user_name", "phong.vh");
                 i.putExtra("password", "123456");
-                startActivity(i);
+//                startActivity(i);
+                startActivityForResult(i, LOGIN_REQUEST);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOGIN_REQUEST && resultCode == RESULT_OK) {
+//            txtConfirm.setText(data.getStringExtra("confirm"));
+        }
     }
 
     @Override
